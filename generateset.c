5a6cbed637a89fbea61960d0b5f17af9a9e61691
sanjai <sanjusanjai2002@gmail.com>
2019-11-28 02:02:10 +0530

    made the array as integer and added the valid move checking feature

--- generateset.c
+++ generateset.c
@@ -3,9 +3,8 @@
 #include<time.h>
 void generateset(long long int n)     //function to generate the set of possibilities
 {
-	long long int x,i,j,flag=1,redcheck=0,endcheck=0;
-	char tic[10];                     //using linear array as it would be easier.array stores the moves done sequentially
-	tic[9]='\0';                      //the last character is always \0
+	long long int x,i,j,flag=1,redcheck=0,endcheck=0,validcheck=0;
+	int tic[9];                     //using linear array as it would be easier.array stores the moves done sequentially
 	for(i=0;i<n;i++)
 	{
 reset:
@@ -15,25 +14,38 @@ reset:
 		}
 		for(j=0;j<9;j++)
 		{
+notvalid:
 			x=rand()%9;
 			if(flag==1)                  //flag checks whose move it is and assigns the respective value to it
 			{                            //flag==1 means it is player 1's move 
+				validcheck=valid(x);
+				if(validcheck==1)
+				{
+					goto notvalid;
+				}
 				tic[j]=x;
 				flag=2;                  //setting flag to other player
 			}
 			else
 			{
+				validcheck=valid(x);
+				if(validcheck==1)
+				{
+					goto notvalid;
+				}
 				tic[j]=x;                //player 2's move
 				flag=1;
 			}
 			redcheck=check(tic);              //check for redundancies in the current state of the game
 			if(redcheck==1)
 			{
-				goto reset;                   //if redundancy is detected reset to start again
+				i++;
+				goto reset;                   //if redundancy is detected reset to start again i++ is to not let redundancies to let it go in infinite loop
 			}
 			endcheck=end(tic);                //check if the game has ended
 			if(endcheck==1)
 			{
+				i++;
 				store(tic);                   //if game has ended then store it and break out of the loop
 				break;
 			}
@@ -48,5 +60,6 @@ int main()
 	printf("how many times do u want to generate it?");
 	scanf("%lld",&n);
 	generateset(n);
+	printf("\ngenerated the file for ")
 	return 0;
 }
