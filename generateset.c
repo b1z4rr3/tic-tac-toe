cbb59950a43fee269a346962366e3d3bc1192b3f
sanjai <sanjusanjai2002@gmail.com>
2019-11-28 01:30:40 +0530

    done the pre generate test code

---
+++ generateset.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void generateset(long long int n)     //function to generate the set of possibilities
+{
+	long long int x,i,j,flag=1,redcheck=0,endcheck=0;
+	char tic[10];                     //using linear array as it would be easier
+	tic[9]='\0';                      //the last character is always \0
+	for(i=0;i<n;i++)
+	{
+reset:
+		for(j=0;j<9;j++)                  //loop to reset the values of the array to 0
+		{
+			tic[j]=0;
+		}
+		for(j=0;j<9;j++)
+		{
+			x=rand()%9;
+			if(flag==1)                  //flag checks whose move it is and assigns the respective value to it
+			{                            //falg==1 means it is player 1's move 
+				tic[i]=1;
+				flag=2;                  //setting flag to other player
+			}
+			else
+			{
+				tic[i]=2;                //player 2's move
+				flag=1;
+			}
+			redcheck=check(tic);              //check for redundancies in the current state of the game
+			if(redcheck==1)
+			{
+				goto reset;                   //if redundancy is detected reset to start again
+			}
+			endcheck=end(tic);                //check if the game has ended
+			if(endcheck==1)
+			{
+				store(tic);                   //if game has ended then store it and break out of the loop
+				break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	long long int n;
+	srand(time(0));
+	printf("how many times do u want to generate it?");
+	scanf("%lld",&n);
+	generateset(n);
+	return 0;
+}
